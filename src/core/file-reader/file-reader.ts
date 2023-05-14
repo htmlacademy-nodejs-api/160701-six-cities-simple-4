2313b1f5e6a6cfbc3f5d1c6025ae2570d66a6dee
Дмитрий <shcheklein.dev@gmail.com>
2023-05-14 20:59:12 +0300

    2.15. Добавит потоки (stream). WIP

--- src/core/file-reader/file-reader.ts
+++ src/core/file-reader/file-reader.ts
@@ -1,12 +1,36 @@
 import { FileReaderInterface } from './file-reader.interface.js';
 import EventEmitter from 'node:events';
+import { createReadStream } from 'node:fs';
 
+const CHUNK_SIZE = 2 ** 14; // 16KB
 export default class TSVFileReader extends EventEmitter implements FileReaderInterface {
   constructor(public filename: string) {
     super();
   }
 
   public async read(): Promise<void> {
-    // Код для работы с потоками
+    const steram = createReadStream(this.filename, {
+      highWaterMark: CHUNK_SIZE,
+      encoding: 'utf-8',
+    });
+    let remainingData = '';
+    let nextLinePosition = -1;
+    let importedRowCount = 0;
+
+    for await (const chunk of steram) {
+      remainingData += chunk.toString();
+      nextLinePosition = remainingData.indexOf('\n');
+
+      while (nextLinePosition >= 0) {
+        const completeRow = remainingData.slice(0, nextLinePosition + 1);
+        remainingData = remainingData.slice(++nextLinePosition);
+
+        importedRowCount++;
+
+        this.emit('readline', completeRow);
+      }
+    }
+
+    this.emit('end', importedRowCount);
   }
 }

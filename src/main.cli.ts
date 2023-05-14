936f8663717607fa28005491cf3f1564b8ab2826
Дмитрий <shcheklein.dev@gmail.com>
2023-05-14 08:36:19 +0300

    1.14. Зарегистрируем команду ImportCommand.

--- src/main.cli.ts
+++ src/main.cli.ts
@@ -1,8 +1,9 @@
 import CLIApplication from './app/cli.js';
 import HelpCommand from './core/cli-command/help.command.js';
+import ImportCommand from './core/cli-command/import.command.js';
 import VersionCommand from './core/cli-command/version.command.js';
 
 const myManager = new CLIApplication();
 
-myManager.registerCommands([new HelpCommand(), new VersionCommand()]);
+myManager.registerCommands([new HelpCommand(), new VersionCommand(), new ImportCommand()]);
 myManager.processCommand(process.argv);

8b2860fd7a748a78ac85d1cc415070febbb1e42e
Дмитрий <shcheklein.dev@gmail.com>
2023-05-14 20:48:47 +0300

    2.14. Рефакторит класс TSVFileReader. WIP

--- src/core/file-reader/file-reader.ts
+++ src/core/file-reader/file-reader.ts
@@ -1,12 +1,12 @@
-import { readFileSync } from 'node:fs';
 import { FileReaderInterface } from './file-reader.interface.js';
+import EventEmitter from 'node:events';
 
-export default class TSVFileReader implements FileReaderInterface {
-  private rawData = '';
+export default class TSVFileReader extends EventEmitter implements FileReaderInterface {
+  constructor(public filename: string) {
+    super();
+  }
 
-  constructor(public filename: string) {}
-
-  public read(): void {
-    this.rawData = readFileSync(this.filename, { encoding: 'utf-8' });
+  public async read(): Promise<void> {
+    // Код для работы с потоками
   }
 }

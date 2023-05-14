13629ce9e986a801df2bb29eb17004f8c9a3d5f5
Дмитрий <shcheklein.dev@gmail.com>
2023-05-14 09:33:50 +0300

    Fix: TSVFileReader

--- src/core/file-reader/file-reader.ts
+++ src/core/file-reader/file-reader.ts
@@ -16,53 +16,71 @@ export default class TSVFileReader implements FileReaderInterface {
     if (!this.rawData) {
       return [];
     }
-
-    return this.rawData
+    const lines = this.rawData
       .split('\n')
       .filter((row) => row.trim() !== '')
-      .map((line) => line.split('\t'))
-      .map((data) => {
-        const [
-          title,
-          description,
-          createdDate,
-          city,
-          preview,
-          images,
-          isPremium,
-          rating,
-          type,
-          rooms,
-          guests,
-          price,
-          features,
+      .map((line) => line.split('\t'));
+
+    return lines.map((data) => {
+      const [
+        title,
+        description,
+        createdDate,
+        city,
+        preview,
+        images,
+        isPremium,
+        rating,
+        type,
+        rooms,
+        guests,
+        price,
+        features,
+        firstName,
+        email,
+        avatarPath,
+        password,
+        userType,
+        commentsCount,
+        coordinates,
+      ] = data;
+      const [latitude = null, longitude = null] = this.parseGroup(coordinates);
+
+      return {
+        title,
+        description,
+        createdAt: new Date(createdDate),
+        city,
+        preview,
+        images: this.parseGroup(images),
+        isPremium: !this.isEmpty(isPremium),
+        rating: Number(rating),
+        type: type as OfferType,
+        rooms: Number(rooms),
+        guests: Number(guests),
+        price: Number(price),
+        features: this.parseGroup(features) as OfferFeatures[],
+        author: {
           firstName,
           email,
-          avatarPath,
-          userType,
-          commentsCount,
-          coordinates,
-        ] = data;
-        const [latitude, longitude] = coordinates.split(';');
+          avatarPath: this.isEmpty(avatarPath) ? undefined : avatarPath,
+          password,
+          type: userType as UserRole,
+        },
+        commentsCount: Number(commentsCount),
+        coordinates: { latitude: Number(latitude), longitude: Number(longitude) },
+      };
+    });
+  }
 
-        return {
-          title,
-          description,
-          createdAt: new Date(createdDate),
-          city,
-          preview,
-          images: images.split(';'),
-          isPremium: Boolean(isPremium),
-          rating: Number(rating),
-          type: type as OfferType,
-          rooms: Number(rooms),
-          guests: Number(guests),
-          price: Number(price),
-          features: features.split(';') as OfferFeatures[],
-          author: { firstName, email, avatarPath, type: userType as UserRole },
-          commentsCount: Number(commentsCount),
-          coordinates: { latitude: Number(latitude), longitude: Number(longitude) },
-        };
-      });
+  private isEmpty(field: string): boolean {
+    return field === '-';
+  }
+
+  private parseGroup(str: string): string[] {
+    if (!str) {
+      return [];
+    }
+    return str.split(';');
   }
 }
--- src/types/user.type.ts
+++ src/types/user.type.ts
@@ -4,6 +4,7 @@ export type UserRole = (typeof userRoles)[number];
 export type User = {
   firstName: string;
   email: string;
-  avatarPath: string;
+  avatarPath?: string;
   type: UserRole;
+  password: string;
 };

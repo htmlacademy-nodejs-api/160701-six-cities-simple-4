ed095095ed9c95cf252459ee38dd17a75ec65027
Дмитрий <shcheklein.dev@gmail.com>
2023-05-15 08:47:35 +0300

    Fix: TSVFileReader

--- src/core/file-reader/file-reader.ts
+++ src/core/file-reader/file-reader.ts
@@ -2,26 +2,26 @@ import { FileReaderInterface } from './file-reader.interface.js';
 import EventEmitter from 'node:events';
 import { createReadStream } from 'node:fs';
 
-const CHUNK_SIZE = 2 ** 14; // 16KB
+const KB16 = 2 ** 14;
+
 export default class TSVFileReader extends EventEmitter implements FileReaderInterface {
   constructor(public filename: string) {
     super();
   }
 
   public async read(): Promise<void> {
-    const steram = createReadStream(this.filename, {
-      highWaterMark: CHUNK_SIZE,
+    const stream = createReadStream(this.filename, {
+      highWaterMark: KB16,
       encoding: 'utf-8',
     });
     let remainingData = '';
     let nextLinePosition = -1;
     let importedRowCount = 0;
 
-    for await (const chunk of steram) {
+    for await (const chunk of stream) {
       remainingData += chunk.toString();
-      nextLinePosition = remainingData.indexOf('\n');
 
-      while (nextLinePosition >= 0) {
+      while ((nextLinePosition = remainingData.indexOf('\n')) >= 0) {
         const completeRow = remainingData.slice(0, nextLinePosition + 1);
         remainingData = remainingData.slice(++nextLinePosition);
 

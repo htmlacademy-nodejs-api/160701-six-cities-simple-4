3c5dadbaaf3b60f5f4b76d767596b9c19e8ea2c5
Дмитрий <shcheklein.dev@gmail.com>
2023-05-15 10:38:12 +0300

    3.11. Добавит ConfigService в Application.

--- src/app/rest.ts
+++ src/app/rest.ts
@@ -1,9 +1,11 @@
+import { ConfigInterface } from '../core/config/config.interface.js';
 import { LoggerInterface } from '../core/logger/logger.interface.js';
 
 export default class RestApplication {
-  constructor(private readonly logger: LoggerInterface) {}
+  constructor(private readonly logger: LoggerInterface, private readonly config: ConfigInterface) {}
 
   public async init() {
     this.logger.info('Application initialization…');
+    this.logger.info(`Get value from env $PORT: ${this.config.get('PORT')}`);
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import RestApplication from './app/rest.js';
+import ConfigService from './core/config/config.service.js';
 import PinoService from './core/logger/pino.service.js';
 
 async function bootstrap() {
   const logger = new PinoService();
+  const config = new ConfigService(logger);
 
-  const app = new RestApplication(logger);
+  const app = new RestApplication(logger, config);
   await app.init();
 }
 bootstrap();

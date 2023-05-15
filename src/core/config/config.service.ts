1e2fde5c91c764797a60c43c81a1c63dbd213458
Дмитрий <shcheklein.dev@gmail.com>
2023-05-15 10:24:39 +0300

    3.8. Добавит класс ConfigService.

---
+++ src/core/config/config.service.ts
@@ -0,0 +1,9 @@
+import { ConfigInterface } from './config.interface.js';
+
+export default class ConfigService implements ConfigInterface {
+  constructor(private readonly config: NodeJS.ProcessEnv) {}
+
+  public get(key: string): string | undefined {
+    return this.config[key];
+  }
+}

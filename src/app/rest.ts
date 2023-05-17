9fd6630c1a985630c7f4a2b13f3bcce215c80cba
Дмитрий <shcheklein.dev@gmail.com>
2023-05-17 09:59:12 +0300

    3.22. Внедрит зависимости в RestApplication.

--- src/app/rest.ts
+++ src/app/rest.ts
@@ -1,11 +1,14 @@
+import { inject, injectable } from 'inversify';
 import { ConfigInterface } from '../core/config/config.interface.js';
 import { RestSchema } from '../core/config/rest.schema.js';
 import { LoggerInterface } from '../core/logger/logger.interface.js';
+import { AppComponent } from '../types/app-component.enum.js';
 
+@injectable()
 export default class RestApplication {
   constructor(
-    private readonly logger: LoggerInterface,
-    private readonly config: ConfigInterface<RestSchema>,
+    @inject(AppComponent.LoggerInterface) private readonly logger: LoggerInterface,
+    @inject(AppComponent.ConfigInterface) private readonly config: ConfigInterface<RestSchema>,
   ) {}
 
   public async init() {

0fe2b9fba82ab39b21e2bfeed1f2a9265b62cacf
Дмитрий <shcheklein.dev@gmail.com>
2023-05-17 09:55:08 +0300

    3.19. Добавит контейнер с зависимостями. WIP

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,20 @@
+import 'reflect-metadata';
+import { Container } from 'inversify';
 import RestApplication from './app/rest.js';
 import ConfigService from './core/config/config.service.js';
 import PinoService from './core/logger/pino.service.js';
+import { AppComponent } from './types/app-component.enum.js';
+import { ConfigInterface } from './core/config/config.interface.js';
+import { RestSchema } from './core/config/rest.schema.js';
+import { LoggerInterface } from './core/logger/logger.interface.js';
 
 async function bootstrap() {
-  const logger = new PinoService();
-  const config = new ConfigService(logger);
+  const container = new Container();
+  container.bind<RestApplication>(AppComponent.RestApplication).to(RestApplication);
+  container.bind<ConfigInterface<RestSchema>>(AppComponent.ConfigInterface).to(ConfigService);
+  container.bind<LoggerInterface>(AppComponent.LoggerInterface).to(PinoService);
 
-  const app = new RestApplication(logger, config);
+  const app = container.get<RestApplication>(AppComponent.RestApplication);
   await app.init();
 }
 bootstrap();

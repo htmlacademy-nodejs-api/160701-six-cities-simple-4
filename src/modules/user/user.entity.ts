2db65702d0d610480a4172d0e89068a9b40496dc
Дмитрий <shcheklein.dev@gmail.com>
2023-05-21 20:46:59 +0300

    4.20. Добавит методы в UserEntity. WIP

--- src/modules/user/user.entity.ts
+++ src/modules/user/user.entity.ts
@@ -1,13 +1,19 @@
+import { createSHA256 } from '../../core/helpers/common.js';
 import { User, userRoles } from '../../types/user.type.js';
 const [defaultUser] = userRoles;
 import typegoose, { defaultClasses, getModelForClass } from '@typegoose/typegoose';
 
-const { prop } = typegoose;
+const { prop, modelOptions } = typegoose;
 
 export interface UserEntity extends defaultClasses.Base {}
 
+@modelOptions({
+  schemaOptions: {
+    collection: 'users',
+  },
+})
 export class UserEntity extends defaultClasses.TimeStamps implements User {
-  @prop({ required: true })
+  @prop({ required: true, default: '' })
   public firstName = '';
 
   @prop({ unique: true, required: true })
@@ -22,8 +28,25 @@ export class UserEntity extends defaultClasses.TimeStamps implements User {
   })
   public type = defaultUser;
 
-  @prop({ required: false, default: '' })
-  public password = '';
+  @prop({ required: true, default: '' })
+  public password?: string;
+
+  constructor({ email, firstName, avatarPath, type }: User) {
+    super();
+
+    this.email = email;
+    this.firstName = firstName;
+    this.avatarPath = avatarPath;
+    // this.type = type;
+  }
+
+  public setPassword(password: string, salt: string) {
+    this.password = createSHA256(password, salt);
+  }
+
+  public getPassword() {
+    return this.password;
+  }
 }
 
 export const UserModel = getModelForClass(UserEntity);
--- src/types/user.type.ts
+++ src/types/user.type.ts
@@ -4,7 +4,6 @@ export type UserRole = (typeof userRoles)[number];
 export type User = {
   firstName: string;
   email: string;
-  avatarPath?: string;
+  avatarPath: string;
   type: UserRole;
-  password: string;
 };

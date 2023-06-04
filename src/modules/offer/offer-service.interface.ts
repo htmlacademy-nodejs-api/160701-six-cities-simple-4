64c3d8cb5351d7dfa1940e43dd8a5b9f1a4c37e1
Дмитрий <shcheklein.dev@gmail.com>
2023-06-04 19:36:36 +0300

    5.9. Расширит интерфейс OfferService.

--- src/modules/offer/offer-service.interface.ts
+++ src/modules/offer/offer-service.interface.ts
@@ -1,8 +1,16 @@
 import { DocumentType } from '@typegoose/typegoose';
 import CreateOfferDto from './dto/create-offer.dto.js';
 import { OfferEntity } from './offer.entity.js';
+import UpdateOfferDto from './dto/update-offer.dto.js';
 
 export interface OfferServiceInterface {
   create(dto: CreateOfferDto): Promise<DocumentType<OfferEntity>>;
   findById(id: string): Promise<DocumentType<OfferEntity> | null>;
+  find(): Promise<DocumentType<OfferEntity>[]>;
+  findByCityId(cityId: string, count?: number): Promise<DocumentType<OfferEntity>[]>;
+  deleteById(id: string): Promise<DocumentType<OfferEntity> | null>;
+  updateById(id: string, dto: UpdateOfferDto): Promise<DocumentType<OfferEntity> | null>;
+  incCommentCount(id: string): Promise<DocumentType<OfferEntity> | null>;
+  findPremium(): Promise<DocumentType<OfferEntity>[]>;
+  findFavorite(): Promise<DocumentType<OfferEntity>[]>;
 }

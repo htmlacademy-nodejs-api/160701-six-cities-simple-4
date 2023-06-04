7f51d2e6181ee7f3e17712f32967cdd81f770dea
Дмитрий <shcheklein.dev@gmail.com>
2023-06-04 20:59:35 +0300

    5.13. Добавит CommentService.

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,14 @@ import { createApiApplicationContainer } from './app/api.container.js';
 import { Container } from 'inversify';
 import { createUserContainer } from './modules/user/user.container.js';
 import { createOfferContainer } from './modules/offer/offer.container.js';
+import { createCommentContainer } from './modules/comment/comment.container.js';
 
 async function bootstrap() {
   const mainContainer = Container.merge(
     createApiApplicationContainer(),
     createUserContainer(),
     createOfferContainer(),
+    createCommentContainer(),
   );
   const app = mainContainer.get<ApiApplication>(AppComponent.RestApplication);
   await app.init();

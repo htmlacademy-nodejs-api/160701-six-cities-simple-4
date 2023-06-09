8a4ed4c3915dbd1be8acc18fabf9d1bd0f3691a4
Dmitry <sheklein.dima@gmail.com>
2023-06-09 19:24:45 +0300

    6.11. Добавит код обработчика index в CityController.

--- src/modules/city/city.controller.ts
+++ src/modules/city/city.controller.ts
@@ -1,20 +1,26 @@
 import { injectable, inject } from 'inversify';
-import { Controller } from '../../core/controller/controller.abstract';
-import { AppComponent } from '../../types/app-component.enum';
+import { Controller } from '../../core/controller/controller.abstract.js';
+import { AppComponent } from '../../types/app-component.enum.js';
 import { LoggerInterface } from '../../core/logger/logger.interface';
 import { HttpMethod } from '../../types/http-method.enum.js';
 import { Request, Response } from 'express';
+import { CityServiceInterface } from './city-service.interface';
 
 @injectable()
 export default class CityController extends Controller {
-  constructor(@inject(AppComponent.LoggerInterface) logger: LoggerInterface) {
+  constructor(
+    @inject(AppComponent.LoggerInterface) protected readonly logger: LoggerInterface,
+    @inject(AppComponent.CityServiceInterface) protected readonly cityService: CityServiceInterface,
+  ) {
     super(logger);
     this.logger.info('Register routes for CityController');
 
     this.addRoute({ path: '/', method: HttpMethod.Get, handler: this.index });
   }
 
-  public index(req: Request, res: Response): void {
-    // Код обработчика
+  public async index(_req: Request, res: Response): Promise<void> {
+    const cities = await this.cityService.find();
+
+    this.ok(res, cities);
   }
 }

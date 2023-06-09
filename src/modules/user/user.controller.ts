87a6460de10f64bdcb1fcc0aff631b1ba78d874e
Dmitry <sheklein.dima@gmail.com>
2023-06-09 20:47:33 +0300

    6.28. Добавит создание пользователей в UserController.

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -4,21 +4,39 @@ import { Controller } from '../../core/controller/controller.abstract.js';
 import { LoggerInterface } from '../../core/logger/logger.interface.js';
 import { AppComponent } from '../../types/app-component.enum.js';
 import { HttpMethod } from '../../types/http-method.enum.js';
+import { ConfigInterface } from '../../core/config/config.interface.js';
+import { RestSchema } from '../../core/config/rest.schema.js';
+import { UserServiceInterface } from './user-service.interface.js';
+import HttpError from '../../core/errors/http-error.js';
+import { StatusCodes } from 'http-status-codes';
+import { fillDTO } from '../../core/helpers/common.js';
+import UserRdo from './rdo/user.rdo.js';
 import CreateUserDto from './dto/create-user.dto.js';
 
 @injectable()
 export default class UserController extends Controller {
-  constructor(@inject(AppComponent.LoggerInterface) protected readonly logger: LoggerInterface) {
+  constructor(
+    @inject(AppComponent.LoggerInterface) protected readonly logger: LoggerInterface,
+    @inject(AppComponent.ConfigInterface) private readonly configService: ConfigInterface<RestSchema>,
+    @inject(AppComponent.UserServiceInterface) private readonly userService: UserServiceInterface,
+  ) {
     super(logger);
     this.logger.info('Register routes for UserController…');
 
     this.addRoute({ path: '/register', method: HttpMethod.Post, handler: this.create });
   }
 
-  public create(
-    _req: Request<Record<string, unknown>, Record<string, unknown>, CreateUserDto>,
-    _res: Response,
-  ): void {
-    throw new Error('[UserController] Oops');
+  public async create(
+    { body }: Request<Record<string, unknown>, Record<string, unknown>, CreateUserDto>,
+    res: Response,
+  ): Promise<void> {
+    const existsUser = await this.userService.findByEmail(body.email);
+
+    if (existsUser) {
+      throw new HttpError(StatusCodes.CONFLICT, `User with email «${body.email}» exists.`, 'UserController');
+    }
+
+    const result = await this.userService.create(body, this.configService.get('SALT'));
+    this.created(res, fillDTO(UserRdo, result));
   }
 }

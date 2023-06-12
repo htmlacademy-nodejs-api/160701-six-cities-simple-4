9c17e348fce088bc757733c55ffda4d28cb1e498
Дмитрий <shcheklein.dev@gmail.com>
2023-06-12 15:17:56 +0300

    7.8. Добавит метод getOffersFromCity в CityController.

--- src/modules/city/city.controller.ts
+++ src/modules/city/city.controller.ts
@@ -7,22 +7,39 @@ import { Request, Response } from 'express';
 import { CityServiceInterface } from './city-service.interface';
 import { fillDTO } from '../../core/helpers/index.js';
 import CityRdo from './rdo/city.rdo.js';
+import { OfferServiceInterface } from '../offer/offer-service.interface.js';
+import * as core from 'express-serve-static-core';
+import { RequestQuery } from '../../types/request-query.type.js';
+import OfferRdo from '../offer/rdo/offer.rdo.js';
 
+type ParamsGetCity = {
+  cityId: string;
+};
 @injectable()
 export default class CityController extends Controller {
   constructor(
     @inject(AppComponent.LoggerInterface) protected readonly logger: LoggerInterface,
     @inject(AppComponent.CityServiceInterface) protected readonly cityService: CityServiceInterface,
+    @inject(AppComponent.OfferServiceInterface) private readonly offerService: OfferServiceInterface,
   ) {
     super(logger);
     this.logger.info('Register routes for CityController');
 
     this.addRoute({ path: '/', method: HttpMethod.Get, handler: this.index });
+    this.addRoute({ path: '/:cityId/offers', method: HttpMethod.Get, handler: this.getOffersFromCity });
   }
 
   public async index(_req: Request, res: Response): Promise<void> {
     const cities = await this.cityService.find();
 
     this.ok(res, fillDTO(CityRdo, cities));
+  }
+
+  public async getOffersFromCity(
+    { params, query }: Request<core.ParamsDictionary | ParamsGetCity, unknown, unknown, RequestQuery>,
+    res: Response,
+  ): Promise<void> {
+    const offers = await this.offerService.findByCityId(params.cityId, query.limit);
+    this.ok(res, fillDTO(OfferRdo, offers));
   }
 }

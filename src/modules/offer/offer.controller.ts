e275746a9d7d21b604ed7757b32dc3b7a2ce07ea
Дмитрий <shcheklein.dev@gmail.com>
2023-06-12 14:15:09 +0300

    7.2. Добавит обработчик show (OfferController). WIP

--- src/modules/offer/offer.controller.ts
+++ src/modules/offer/offer.controller.ts
@@ -6,17 +6,36 @@ import { AppComponent } from '../../types/app-component.enum.js';
 import { LoggerInterface } from '../../core/logger/logger.interface';
 import { HttpMethod } from '../../types/http-method.enum.js';
 import HttpError from '../../core/errors/http-error.js';
+import { OfferServiceInterface } from './offer-service.interface.js';
+import * as core from 'express-serve-static-core';
+
+type ParamsGetOffer = {
+  offerId: string;
+};
 
 @injectable()
 export default class OfferController extends Controller {
-  constructor(@inject(AppComponent.LoggerInterface) logger: LoggerInterface) {
+  constructor(
+    @inject(AppComponent.LoggerInterface) logger: LoggerInterface,
+    @inject(AppComponent.OfferServiceInterface) private readonly offerService: OfferServiceInterface,
+  ) {
     super(logger);
 
     this.logger.info('Register routes for OfferController…');
     this.addRoute({ path: '/:offerId', method: HttpMethod.Get, handler: this.show });
   }
 
-  public async show(_req: Request, _res: Response): Promise<void> {
-    throw new HttpError(StatusCodes.NOT_IMPLEMENTED, 'Not implemented', 'OfferController');
+  public async show(
+    { params }: Request<core.ParamsDictionary | ParamsGetOffer>,
+    res: Response,
+  ): Promise<void> {
+    const { offerId } = params;
+    const offer = await this.offerService.findById(offerId);
+
+    if (!offer) {
+      throw new HttpError(StatusCodes.NOT_FOUND, `Offer with id ${offerId} not found.`, 'OfferController');
+    }
+
+    this.ok(res, offer);
   }
 }

e51fb32de2ed72ba56c5be9a8ac9abf850b41bcb
Dmitry <sheklein.dima@gmail.com>
2023-06-14 15:23:57 +0300

    7.22. Добавит валидацию для CreateUserDto.

--- src/modules/user/dto/create-user.dto.ts
+++ src/modules/user/dto/create-user.dto.ts
@@ -1,6 +1,22 @@
+import { IsEmail, IsString, Length } from 'class-validator';
+import { UserNameLength, UserPasswordLength } from '../../../const/validation.js';
+
 export default class CreateUserDto {
+  @IsEmail({}, { message: 'email must be valid address' })
   public email!: string;
+
+  @IsString({ message: 'firstname is required' })
+  @Length(UserNameLength.Min, UserNameLength.Max, {
+    message: `Min length is ${UserNameLength.Min}, max is ${UserNameLength.Max}`,
+  })
   public firstName!: string;
+
+  @IsString({ message: 'avatarPath is required' })
   public avatarPath!: string;
+
+  @IsString({ message: 'password is required' })
+  @Length(UserPasswordLength.Min, UserPasswordLength.Max, {
+    message: `Min length for password is ${UserPasswordLength.Min}, max is ${UserPasswordLength.Max}`,
+  })
   public password!: string;
 }
--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -13,6 +13,7 @@ import { fillDTO } from '../../core/helpers/common.js';
 import UserRdo from './rdo/user.rdo.js';
 import CreateUserDto from './dto/create-user.dto.js';
 import LoginUserDto from './dto/login-user.dto.js';
+import { ValidateDtoMiddleware } from '../../common/middlewares/validate-dto.middleware.js';
 
 @injectable()
 export default class UserController extends Controller {
@@ -24,7 +25,12 @@ export default class UserController extends Controller {
     super(logger);
     this.logger.info('Register routes for UserController…');
 
-    this.addRoute({ path: '/register', method: HttpMethod.Post, handler: this.create });
+    this.addRoute({
+      path: '/register',
+      method: HttpMethod.Post,
+      handler: this.create,
+      middlewares: [new ValidateDtoMiddleware(CreateUserDto)],
+    });
     this.addRoute({ path: '/login', method: HttpMethod.Post, handler: this.login });
   }
 

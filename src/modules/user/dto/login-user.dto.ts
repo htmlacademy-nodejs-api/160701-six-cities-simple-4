40355d5a46e87874ae467560c5a9cea812324c1d
Dmitry <sheklein.dima@gmail.com>
2023-06-14 15:57:01 +0300

    7.23. Добавит валидацию для LoginUserDto.

--- src/modules/user/dto/login-user.dto.ts
+++ src/modules/user/dto/login-user.dto.ts
@@ -1,4 +1,13 @@
+import { IsEmail, IsString, Length } from 'class-validator';
+import { UserPasswordLength } from '../../../const/validation.js';
+
 export default class LoginUserDto {
+  @IsEmail({}, { message: 'email must be a valid address' })
   public email!: string;
+
+  @IsString({ message: 'password is required' })
+  @Length(UserPasswordLength.Min, UserPasswordLength.Max, {
+    message: `Min length for password is ${UserPasswordLength.Min}, max is ${UserPasswordLength.Max}`,
+  })
   public password!: string;
 }

8c3e85726d3db3faafefa1324168fa1a0804a468
Dmitry <sheklein.dima@gmail.com>
2023-06-15 18:46:12 +0300

    8.4. Добавит метод verifyUser в UserService.

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -6,6 +6,7 @@ import { inject, injectable } from 'inversify';
 import { AppComponent } from '../../types/app-component.enum.js';
 import { LoggerInterface } from '../../core/logger/logger.interface.js';
 import UpdateUserDto from './dto/update-user.dto.js';
+import LoginUserDto from './dto/login-user.dto.js';
 
 @injectable()
 export default class UserService implements UserServiceInterface {
@@ -40,5 +41,11 @@ export default class UserService implements UserServiceInterface {
 
   public async updateById(userId: string, dto: UpdateUserDto): Promise<DocumentType<UserEntity> | null> {
     return this.userModel.findByIdAndUpdate(userId, dto, { new: true }).exec();
+  }
+
+  public async verifyUser(dto: LoginUserDto, salt: string): Promise<DocumentType<UserEntity> | null> {
+    const user = await this.findByEmail(dto.email);
+
+    return user?.verifyPassword(dto.password, salt) ? user : null;
   }
 }

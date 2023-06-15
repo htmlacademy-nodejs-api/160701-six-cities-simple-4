14a8b7d2c0e544619dcdd68d748d58f4a93518e3
Dmitry <sheklein.dima@gmail.com>
2023-06-15 13:39:04 +0300

    Унаследует интерфейс, добавит middleware для проверки документа в коллекции.

--- src/modules/city/city-service.interface.ts
+++ src/modules/city/city-service.interface.ts
@@ -1,12 +1,12 @@
 import { DocumentType } from '@typegoose/typegoose';
 import { CityEntity } from './city.entity.js';
 import CreateCityDto from './dto/create-city.dto.js';
+import { DocumentExistsInterface } from '../../types/document-exists.interface.js';
 
-export interface CityServiceInterface {
+export interface CityServiceInterface extends DocumentExistsInterface {
   create(dto: CreateCityDto): Promise<DocumentType<CityEntity>>;
   findByCityId(cityId: string): Promise<DocumentType<CityEntity> | null>;
   findByCityName(cityName: string): Promise<DocumentType<CityEntity> | null>;
   findByCityNameOrCreate(cityName: string, dto: CreateCityDto): Promise<DocumentType<CityEntity>>;
   find(): Promise<DocumentType<CityEntity>[]>;
-  exists(documentId: string): Promise<boolean>;
 }

942381ab8aad55516c06d8184c75761ce899bdb1
Дмитрий <shcheklein.dev@gmail.com>
2023-06-18 17:25:52 +0300

    Fix: rdo messages

--- src/modules/comment/dto/create-comment.dto.ts
+++ src/modules/comment/dto/create-comment.dto.ts
@@ -2,19 +2,19 @@ import { IsMongoId, IsString, Length, IsInt, Min, Max, IsOptional } from 'class-
 import { CommentV } from '../../../const/validation.js';
 
 export default class CreateCommentDto {
-  @IsString({ message: 'text is required' })
-  @Length(CommentV.Text.Min, 1024, { message: 'Min length is 5, max is 1024' })
+  @IsString()
+  @Length(CommentV.Text.Min, CommentV.Text.Max)
   public text!: string;
 
   @IsOptional()
-  @IsMongoId({ message: 'offerId field must be a valid id' })
+  @IsMongoId()
   public offerId!: string;
 
-  @IsMongoId({ message: 'userId field must be a valid id' })
+  @IsMongoId()
   public userId!: string;
 
-  @IsInt({ message: 'Rating must be an integer' })
-  @Min(CommentV.Rating.Min, { message: `Minimum rating is ${CommentV.Rating.Min}` })
-  @Max(CommentV.Rating.Max, { message: `Maximum rating is ${CommentV.Rating.Max}` })
+  @IsInt()
+  @Min(CommentV.Rating.Min)
+  @Max(CommentV.Rating.Max)
   public rating!: number;
 }

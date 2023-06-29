bc45c8527c8751d5b0d393976d1d84b67c3df8cd
Дмитрий <shcheklein.dev@gmail.com>
2023-06-29 09:51:56 +0300

    Fix: offers sort

--- src/modules/offer/offer.constant.ts
+++ src/modules/offer/offer.constant.ts
@@ -5,7 +5,7 @@ export const DEFAULT_MAX_OFFER_COUNT = 500;
 export const DEFAULT_OFFER_PREMIUM_COUNT = 3;
 
 export const OFFER_SORT = {
-  Default: { createdAt: SortType.Down },
+  DateNew: { createdAt: SortType.Down },
   Rated: { rating: SortType.Down },
   Popular: { commentsCount: SortType.Down },
   PriceTop: { price: SortType.Down },
